4dcf03ad0aa464fab75ae66eee368186ee7167ae
Maxime Jezequel <maxime.jezequel@gmail.com>
2021-06-15 14:19:37 +0200

    cowsay initial commmit

---
+++ information.js
@@ -0,0 +1,8 @@
+let name = 'Maxime Jezequel';
+let campus = 'Remote-FR';
+
+const myInfos = () => {
+    console.log(`Hi my name is ${name}, my campus is ${campus}`);
+};
+
+module.exports = myInfos;
