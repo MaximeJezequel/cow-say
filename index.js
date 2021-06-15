f3d749a03d166d6fc14a478a58b0c999f156dd86
Maxime Jezequel <maxime.jezequel@gmail.com>
2021-06-15 15:25:59 +0200

    index + information modified

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const myInformation = require('./information.js');
-myInformation();
+const info = require('./information.js');
+
+let cowsay = require("cowsay");
+    console.log(cowsay.say({
+    text : `Hi, I'm ${info.name} and my campus is ${info.campus}`,
+    e : "oO",
+    T : "U "
+}));
--- information.js
+++ information.js
@@ -1,8 +1,6 @@
-let name = 'Maxime Jezequel';
-let campus = 'Remote-FR';
+const message = {
+    name: 'Maxime Jezequel',
+    campus: "Remote-FR"
+}
 
-const myInfos = () => {
-    console.log(`Hi my name is ${name}, my campus is ${campus}`);
-};
-
-module.exports = myInfos;
+module.exports = message;
